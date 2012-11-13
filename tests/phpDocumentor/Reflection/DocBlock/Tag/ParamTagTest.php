a18985a888a204397bbe5de137ef1f100152814a
Vasil Rangelov <boen.robot@gmail.com>
2012-11-13 21:50:56 +0200

    Doc updates and fixes.

--- tests/phpDocumentor/Reflection/DocBlock/Tag/ParamTagTest.php
+++ tests/phpDocumentor/Reflection/DocBlock/Tag/ParamTagTest.php
@@ -13,7 +13,7 @@
 namespace phpDocumentor\Reflection\DocBlock\Tag;
 
 /**
- * Test class for \phpDocumentor\Reflection\DocBlock\ParamTag.
+ * Test class for \phpDocumentor\Reflection\DocBlock\ParamTag
  *
  * @author    Mike van Riel <mike.vanriel@naenius.com>
  * @copyright 2010-2011 Mike van Riel / Naenius. (http://www.naenius.com)
