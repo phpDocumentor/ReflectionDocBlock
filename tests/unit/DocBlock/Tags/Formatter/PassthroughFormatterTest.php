c3de30dc964a2c67899b6d879ded3537b9269c10
Mike van Riel <mike@ingewikkeld.net>
2015-06-14 00:10:36 +0200

    Add test for passthroughformatter

---
+++ tests/unit/DocBlock/Tags/Formatter/PassthroughFormatterTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection\DocBlock\Tags\Formatter;
+
+use Mockery as m;
+use phpDocumentor\Reflection\DocBlock\Description;
+use phpDocumentor\Reflection\DocBlock\Tags\Other;
+
+/**
+ * @coversDefaultClass \phpDocumentor\Reflection\DocBlock\Tags\Formatter\PassthroughFormatter
+ */
+class PassthroughFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::format
+     * @uses \phpDocumentor\Reflection\DocBlock\Description
+     * @uses \phpDocumentor\Reflection\DocBlock\Tags\BaseTag
+     * @uses \phpDocumentor\Reflection\DocBlock\Tags\Other
+     */
+    public function testFormatterCallsToStringAndReturnsAStandardRepresentation()
+    {
+        $expected = '@unknown-tag This is a description';
+
+        $fixture = new PassthroughFormatter();
+
+        $this->assertSame(
+            $expected,
+            $fixture->format(new Other('unknown-tag', new Description('This is a description')))
+        );
+    }
+}
