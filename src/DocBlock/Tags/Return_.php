fc48be405da8481f3c12f351e6409c0fb678987a
Mike van Riel <mike@ingewikkeld.net>
2015-06-22 06:09:54 +0200

    Written unit tests for return tag

--- src/DocBlock/Tags/Return_.php
+++ src/DocBlock/Tags/Return_.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * phpDocumentor
+ * This file is part of phpDocumentor.
  *
- * PHP Version 5.3
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  *
- * @author    Mike van Riel <mike.vanriel@naenius.com>
- * @copyright 2010-2011 Mike van Riel / Naenius (http://www.naenius.com)
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
@@ -14,13 +14,13 @@ namespace phpDocumentor\Reflection\DocBlock\Tags;
 
 use phpDocumentor\Reflection\DocBlock\Description;
 use phpDocumentor\Reflection\DocBlock\DescriptionFactory;
-use phpDocumentor\Reflection\DocBlock\Type\Collection;
 use phpDocumentor\Reflection\Type;
 use phpDocumentor\Reflection\TypeResolver;
 use phpDocumentor\Reflection\Types\Context;
+use Webmozart\Assert\Assert;
 
 /**
- * Reflection class for a @return tag in a Docblock.
+ * Reflection class for a {@}return tag in a Docblock.
  */
 final class Return_ extends BaseTag
 {
@@ -31,8 +31,8 @@ final class Return_ extends BaseTag
 
     public function __construct(Type $type, Description $description = null)
     {
-        $this->description = $description;
         $this->type = $type;
+        $this->description = $description;
     }
 
     /**
@@ -45,17 +45,15 @@ final class Return_ extends BaseTag
         Context $context = null
     )
     {
+        Assert::string($body);
+        Assert::allNotNull([$typeResolver, $descriptionFactory]);
+
         $parts = preg_split('/\s+/Su', $body, 2);
 
         $type = $typeResolver->resolve(isset($parts[0]) ? $parts[0] : '', $context);
-        $description = $descriptionFactory->create(isset($parts[1]) ? $parts[1] : '');
+        $description = $descriptionFactory->create(isset($parts[1]) ? $parts[1] : '', $context);
 
         return new static($type, $description);
-    }
-
-    public function __toString()
-    {
-        return $this->type . ' ' . $this->description;
     }
 
     /**
@@ -66,5 +64,10 @@ final class Return_ extends BaseTag
     public function getType()
     {
         return $this->type;
+    }
+
+    public function __toString()
+    {
+        return $this->type . ' ' . $this->description;
     }
 }
---
+++ tests/unit/DocBlock/Tags/ReturnTest.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection\DocBlock\Tags;
+
+use Mockery as m;
+use phpDocumentor\Reflection\DocBlock\Description;
+use phpDocumentor\Reflection\DocBlock\DescriptionFactory;
+use phpDocumentor\Reflection\TypeResolver;
+use phpDocumentor\Reflection\Types\Context;
+use phpDocumentor\Reflection\Types\String_;
+
+/**
+ * @coversDefaultClass \phpDocumentor\Reflection\DocBlock\Tags\Return_
+ * @covers ::<private>
+ */
+class ReturnTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @uses   \phpDocumentor\Reflection\DocBlock\Tags\Return_::__construct
+     * @uses   \phpDocumentor\Reflection\DocBlock\Description
+     * @covers \phpDocumentor\Reflection\DocBlock\Tags\BaseTag::getName
+     */
+    public function testIfCorrectTagNameIsReturned()
+    {
+        $fixture = new Return_(new String_(), new Description('Description'));
+
+        $this->assertSame('return', $fixture->getName());
+    }
+
+    /**
+     * @uses   \phpDocumentor\Reflection\DocBlock\Tags\Return_::__construct
+     * @uses   \phpDocumentor\Reflection\DocBlock\Tags\Return_::__toString
+     * @uses   \phpDocumentor\Reflection\DocBlock\Tags\Formatter\PassthroughFormatter
+     * @uses   \phpDocumentor\Reflection\DocBlock\Description
+     * @covers \phpDocumentor\Reflection\DocBlock\Tags\BaseTag::render
+     */
+    public function testIfTagCanBeRenderedUsingDefaultFormatter()
+    {
+        $fixture = new Return_(new String_(), new Description('Description'));
+
+        $this->assertSame('string Description', $fixture->render());
+    }
+
+    /**
+     * @uses   \phpDocumentor\Reflection\DocBlock\Tags\Return_::__construct
+     * @uses   \phpDocumentor\Reflection\DocBlock\Description
+     * @covers \phpDocumentor\Reflection\DocBlock\Tags\BaseTag::render
+     */
+    public function testIfTagCanBeRenderedUsingSpecificFormatter()
+    {
+        $fixture = new Return_(new String_(), new Description('Description'));
+
+        $formatter = m::mock(Formatter::class);
+        $formatter->shouldReceive('format')->with($fixture)->andReturn('Rendered output');
+
+        $this->assertSame('Rendered output', $fixture->render($formatter));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getType
+     */
+    public function testHasType()
+    {
+        $expected = new String_();
+
+        $fixture = new Return_($expected);
+
+        $this->assertSame($expected, $fixture->getType());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers \phpDocumentor\Reflection\DocBlock\Tags\BaseTag::getDescription
+     * @uses   \phpDocumentor\Reflection\DocBlock\Description
+     */
+    public function testHasDescription()
+    {
+        $expected = new Description('Description');
+
+        $fixture = new Return_(new String_(), $expected);
+
+        $this->assertSame($expected, $fixture->getDescription());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::__toString
+     * @uses   \phpDocumentor\Reflection\DocBlock\Description
+     */
+    public function testStringRepresentationIsReturned()
+    {
+        $fixture = new Return_(new String_(), new Description('Description'));
+
+        $this->assertSame('string Description', (string)$fixture);
+    }
+
+    /**
+     * @covers ::create
+     * @uses \phpDocumentor\Reflection\DocBlock\Tags\Return_::<public>
+     * @uses \phpDocumentor\Reflection\DocBlock\DescriptionFactory
+     * @uses \phpDocumentor\Reflection\TypeResolver
+     * @uses \phpDocumentor\Reflection\DocBlock\Description
+     * @uses \phpDocumentor\Reflection\Types\String_
+     * @uses \phpDocumentor\Reflection\Types\Context
+     */
+    public function testFactoryMethod()
+    {
+        $descriptionFactory = m::mock(DescriptionFactory::class);
+        $resolver = new TypeResolver();
+        $context = new Context('');
+
+        $type = new String_();
+        $description = new Description('My Description');
+        $descriptionFactory->shouldReceive('create')->with('My Description', $context)->andReturn($description);
+
+        $fixture = Return_::create('string My Description', $resolver, $descriptionFactory, $context);
+
+        $this->assertSame('string My Description', (string)$fixture);
+        $this->assertEquals($type, $fixture->getType());
+        $this->assertSame($description, $fixture->getDescription());
+    }
+
+    /**
+     * @covers ::create
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFactoryMethodFailsIfBodyIsNotString()
+    {
+        $this->assertNull(Return_::create([]));
+    }
+
+    /**
+     * @covers ::create
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFactoryMethodFailsIfBodyIsNotEmpty()
+    {
+        $this->assertNull(Return_::create(''));
+    }
+}
