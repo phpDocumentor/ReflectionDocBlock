183824db76118b9dddffc7e522b91fa175f75119
Jaapio <jaap@ijaap.nl>
2017-08-04 22:16:41 +0200

    Restores future support for annotations
    In #89 the regex to parse tags was to strict. The first character after a tag
    had to be a single string. Annotations typically start with a parentheses. Only character not
    allowed after a tag is a [.

---
+++ tests/integration/DocblocksWithAnnotationsTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ *  @copyright 2010-2017 Mike van Riel<mike@phpdoc.org>
+ *  @license   http://www.opensource.org/licenses/mit-license.php MIT
+ *  @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection;
+
+/**
+ * @coversNothing
+ */
+final class DocblocksWithAnnotationsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDocblockWithAnnotations()
+    {
+        $docComment = <<<DOCCOMMENT
+            /**
+     * @var \DateTime[]
+     * @Groups({"a", "b"})
+     */
+DOCCOMMENT;
+
+
+        $factory  = DocBlockFactory::createInstance();
+        $docblock = $factory->create($docComment);
+
+        $this->assertCount(2, $docblock->getTags());
+    }
+}
