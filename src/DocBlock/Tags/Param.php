e495766bf76c3d5ae3072bc7c2fad1e3cdea16f8
Mike van Riel <mike@ingewikkeld.net>
2019-12-27 20:44:28 +0100

    Support braces in types for `@param`

--- src/DocBlock/Tags/Param.php
+++ src/DocBlock/Tags/Param.php
@@ -22,14 +22,8 @@ use Webmozart\Assert\Assert;
 /**
  * Reflection class for the {@}param tag in a Docblock.
  */
-final class Param extends BaseTag implements Factory\StaticMethod
+final class Param extends TagWithType implements Factory\StaticMethod
 {
-    /** @var string */
-    protected $name = 'param';
-
-    /** @var Type */
-    private $type;
-
     /** @var string */
     private $variableName = '';
 
@@ -47,6 +41,7 @@ final class Param extends BaseTag implements Factory\StaticMethod
         Assert::string($variableName);
         Assert::boolean($isVariadic);
 
+        $this->name = 'param';
         $this->variableName = $variableName;
         $this->type = $type;
         $this->isVariadic = $isVariadic;
@@ -65,19 +60,25 @@ final class Param extends BaseTag implements Factory\StaticMethod
         Assert::stringNotEmpty($body);
         Assert::allNotNull([$typeResolver, $descriptionFactory]);
 
-        $parts = preg_split('/(\s+)/Su', $body, 3, PREG_SPLIT_DELIM_CAPTURE);
+        list($firstPart, $body) = self::extractTypeFromBody($body);
         $type = null;
+        $parts = preg_split('/(\s+)/Su', $body, 2, PREG_SPLIT_DELIM_CAPTURE);
         $variableName = '';
         $isVariadic = false;
 
         // if the first item that is encountered is not a variable; it is a type
-        if (isset($parts[0]) && (strlen($parts[0]) > 0) && ($parts[0][0] !== '$')) {
-            $type = $typeResolver->resolve(array_shift($parts), $context);
-            array_shift($parts);
+        if ($firstPart && (strlen($firstPart) > 0) && ($firstPart[0] !== '$')) {
+            $type = $typeResolver->resolve($firstPart, $context);
+        } else {
+            // first part is not a type; we should prepend it to the parts array for further processing
+            array_unshift($parts, $firstPart);
         }
 
         // if the next item starts with a $ or ...$ it must be the variable name
-        if (isset($parts[0]) && (strlen($parts[0]) > 0) && ($parts[0][0] === '$' || substr($parts[0], 0, 4) === '...$')) {
+        if (isset($parts[0])
+            && (strlen($parts[0]) > 0)
+            && ($parts[0][0] === '$' || substr($parts[0], 0, 4) === '...$')
+        ) {
             $variableName = array_shift($parts);
             array_shift($parts);
 
@@ -107,16 +108,6 @@ final class Param extends BaseTag implements Factory\StaticMethod
     }
 
     /**
-     * Returns the variable's type or null if unknown.
-     *
-     * @return Type|null
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
      * Returns whether this tag is variadic.
      *
      * @return boolean
@@ -134,8 +125,8 @@ final class Param extends BaseTag implements Factory\StaticMethod
     public function __toString()
     {
         return ($this->type ? $this->type . ' ' : '')
-        . ($this->isVariadic() ? '...' : '')
-        . '$' . $this->variableName
-        . ($this->description ? ' ' . $this->description : '');
+            . ($this->isVariadic() ? '...' : '')
+            . '$' . $this->variableName
+            . ($this->description ? ' ' . $this->description : '');
     }
 }
