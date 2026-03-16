0e00c75801e8179357c52881e545200b16c8b94b
Brent Shaffer <betterbrent@google.com>
2026-03-16 16:14:09 -0700

    fix: add escaped closing docblock to escaped sequences

--- src/DocBlock/DescriptionFactory.php
+++ src/DocBlock/DescriptionFactory.php
@@ -100,8 +100,9 @@ class DescriptionFactory
 
         return Utils::pregSplit(
             '/\{
-                # "{@}" is not a valid inline tag. This ensures that we do not treat it as one, but treat it literally.
-                (?!@\})
+                # "{@}" and "{@*}" are not a valid inline tags. This ensures that we do not treat them as one, but treat
+                # them literally.
+                (?!(?:@\}|@\*\}) )
                 # We want to capture the whole tag line, but without the inline tag delimiters.
                 (\@
                     # Match everything up to the next delimiter.
